4d5d095a84357f5012f5b4f016209474e87a3c4f
agung <guungofficial@gmail.com>
2019-10-24 16:05:23 +0700

    Add files

---
+++ question_lists.js
@@ -0,0 +1,13 @@
+var quiz = [
+    [0,0, "Pakaian yang menutupi bagian bawah tubuh", "CELANA", "datar"],
+    [0,1, "Penampung air", "EMBER", "turun"],
+    [4,1, "Ibukota Latvia", "RIGA", "datar"],
+    [4,4, "Organisme di air", "ALGA", "turun"],
+    [2,1, "Tidak, lain (sinonim)", "BUKAN", "datar"],
+    [5,4, "Ibukota Peru", "LIMA", "datar"],
+    [4,2, "Negara Indonesia", "INDONESIA", "turun"],
+    [7,1, "Makanan saat bulan Ramadan", "KOLAK", "datar"],
+    [7,5, "Anak semut untuk pakan burung", "KROTO", "turun"],
+    [8,5, "Sentuh, pegang", "RABA", "datar"],
+    [1,7, "Tuduhan tanpa bukti", "FITNAH", "turun"],
+];
