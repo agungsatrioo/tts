01e842ea3edcf34480dabb8419a882a203c30530
agung <guungofficial@gmail.com>
2019-10-24 23:33:33 +0700

    modif

--- question_lists.js
+++ question_lists.js
@@ -1,13 +1,22 @@
 var quiz = [
     [0,0, "Pakaian yang menutupi bagian bawah tubuh", "CELANA", "datar"],
     [0,1, "Penampung air", "EMBER", "turun"],
-    [4,1, "Ibukota Latvia", "RIGA", "datar"],
-    [4,4, "Organisme di air", "ALGA", "turun"],
+    [0,3, "Anak Buah Kapal", "ABK", "turun"],
+
+    [1,7, "Tuduhan tanpa bukti", "FITNAH", "turun"],
+
     [2,1, "Tidak, lain (sinonim)", "BUKAN", "datar"],
+    [2,5, "Tidak (inggris)", "NO", "turun"],
+
+    [4,1, "Ibukota Latvia", "RIGA", "datar"],
+    [4,2, "Negara kita", "INDONESIA", "turun"],
+    [4,4, "Organisme di air", "ALGA", "turun"],
+
     [5,4, "Ibukota Peru", "LIMA", "datar"],
-    [4,2, "Negara Indonesia", "INDONESIA", "turun"],
     [7,1, "Makanan saat bulan Ramadan", "KOLAK", "datar"],
     [7,5, "Anak semut untuk pakan burung", "KROTO", "turun"],
     [8,5, "Sentuh, pegang", "RABA", "datar"],
-    [1,7, "Tuduhan tanpa bukti", "FITNAH", "turun"],
+    [8,7, "Selesai, rampung", "BERES", "turun"],
+    [10, 2, "Susun", "SORTIR", "datar"]
+
 ];
